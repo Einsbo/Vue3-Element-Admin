5a11de053e276603cecbe290f46ab8c73aa7ea20
bakashinji <m18753168203@gmail.com>
2023-10-23 17:32:53 +0800

    初始化项目

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
 import { fileURLToPath, URL } from "node:url";
 
+import path from "path";
+
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import VueDevTools from "vite-plugin-vue-devtools";
 
+import AutoImport from "unplugin-auto-import/vite";
+
+const pathSrc = path.resolve(__dirname, "src");
+console.log(pathSrc);
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VueDevTools()],
+  plugins: [
+    vue(),
+    VueDevTools(),
+    AutoImport({
+      imports: ["vue", "vue-router"],
+      dts: path.resolve(pathSrc, "auto-imports.d.ts"),
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
